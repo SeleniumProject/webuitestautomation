4afe7ca62269cf486516fa9b80d0a9cad6d7fea4
Ramyakumari <ramyaramya5082@gmail.com>
2024-01-12 16:11:06 +0530

    commit with test script Register_TC02

--- src/main/java/com/bb/pages/RegisterPage.java
+++ src/main/java/com/bb/pages/RegisterPage.java
@@ -1,5 +1,75 @@
 package com.bb.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+
+
 public class RegisterPage {
 
+	WebDriver driver;
+
+	public RegisterPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public String getTitle() {
+		return driver.getTitle();
+	}
+	
+	/*
+	 * Page Objects
+	 */
+	
+	By registerlink = By.className("ico-register");
+	By radiobtn = By.id("gender-female");
+	By firstnametxt = By.id("FirstName");
+	By lastnametxt = By.id("LastName");
+	By emailadrs = By. id("Email");
+	By passtxt= By.id("Password");
+	By cnfmpasstxt = By.id("ConfirmPassword");
+	By resiterbtn = By.id("register-button");
+	By validMsg = By.xpath("/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div[1]");
+	
+	/* method creation */
+	
+	public void clickOnRegisterLink() {
+		driver.findElement(registerlink).click();
+	}
+	
+	public void clickOnradiobtn() {
+		driver.findElement(radiobtn).click();
+	}
+	
+	public void enterFrstnm(String firstname) {
+		driver.findElement(firstnametxt).sendKeys(firstname);
+	}
+	
+	public void enterLstnm(String lastname) {
+		driver.findElement(lastnametxt).sendKeys(lastname);
+	}
+	
+	public void enterEmailAddress(String emailad) {
+		driver.findElement(emailadrs).sendKeys(emailad);
+	}
+	
+	public void enterpassword(String password) {
+		driver.findElement(passtxt).sendKeys(password);
+	}
+	
+	public void entercnfmpassword(String confirmpass) {
+		driver.findElement(cnfmpasstxt).sendKeys(confirmpass);
+	}
+	
+	public void clickOnRegisterbtn() {
+		driver.findElement(resiterbtn).click();
+	}
+	
+	public void verifySuccesfullMsg() {
+		String successmsg = driver.findElement(validMsg).getText();
+		Assert.assertEquals(successmsg, "Your registration completed");
+	}
+	
 }
+
--- src/test/java/com/bb/tests/RegisterTest.java
+++ src/test/java/com/bb/tests/RegisterTest.java
@@ -1,5 +1,51 @@
 package com.bb.tests;
 
-public class RegisterTest {
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
+import com.bb.base.BasePage;
+import com.bb.pages.RegisterPage;
+
+public class RegisterTest extends BasePage {
+
+	RegisterPage registerpage;
+	
+	@BeforeSuite
+	public void setUp() {
+		invokeBrowser();
+		registerpage = new RegisterPage(driver);
+	}
+	
+	  @DataProvider (name = "register")
+    public Object[][] validLogin(){
+	 return new Object[][] {{"Demo Web Shop","Demo Web Shop. Register","Ramya","kumari","ramya135625886@gmail.com","testing1234","testing1234"}};
+    }
+
+	
+	@Test(dataProvider = "register")
+	public void verifyRegisterPageWithValidCredentials(String HomePageTitle , String RegisterTitle,  String FirstName , String lastName , String EmailAddress , String Password , String ConfirmPassword) {
+		 
+		Assert.assertEquals(registerpage.getTitle(), HomePageTitle);
+		registerpage.clickOnRegisterLink();
+		Assert.assertEquals(registerpage.getTitle(), RegisterTitle );
+		registerpage.clickOnradiobtn();
+		registerpage.enterFrstnm(FirstName);
+		registerpage.enterLstnm(lastName);
+		registerpage.enterEmailAddress(EmailAddress);
+		registerpage.enterpassword(Password);
+		registerpage.entercnfmpassword(ConfirmPassword);
+		registerpage.clickOnRegisterbtn();
+		registerpage.verifySuccesfullMsg();
+	}
+	
+	@AfterSuite
+	
+	public void closeBrowser() {
+		driver.close();
+	}
+	
+		
 }
